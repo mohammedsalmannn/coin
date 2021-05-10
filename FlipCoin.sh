039ab2cb9344bdb2467f878fd52d2eae3a1c89eb
mohammedsalmannn <rmdsalman001@gmail.com>
2021-05-10 21:09:04 +0530

    Added : single_conbination

--- FlipCoin.sh
+++ FlipCoin.sh
@@ -11,3 +11,30 @@ then
 else
         echo "Tail"
 fi
+
+echo "To Store Singlet Combination In A Dcitionary And To Determine Its Percentage"
+
+
+head=1
+
+declare -A siglet
+headCount=0
+tailCount=0
+
+read -p "Enter The Number Of Coin Flip - " coinFlip
+for((index=1; index<=coinFlip; index++))
+do
+	flips=$((RANDOM%2))
+	
+	if [[ $flips -eq $head ]]
+	then
+		singlet[Head]=$((++headCount))
+	else
+		siglet[Tail]=$((++tailCount))
+	fi
+done
+
+percentage_of_head=$(($headCount*100/$coinFlip))
+echo "Percentage Of Head - "$percentage_of_head
+percentage_of_tail=$(( $tailCount*100/$coinFlip ))
+echo "Percentage Of Tail - "$percentage_of_tail
