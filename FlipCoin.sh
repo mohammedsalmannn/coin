966d9bee418edd965e5c37456dbe268ae700f205
mohammedsalmannn <rmdsalman001@gmail.com>
2021-05-10 21:12:58 +0530

    Added: Double_percentage

--- FlipCoin.sh
+++ FlipCoin.sh
@@ -38,3 +38,47 @@ percentage_of_head=$(($headCount*100/$coinFlip))
 echo "Percentage Of Head - "$percentage_of_head
 percentage_of_tail=$(( $tailCount*100/$coinFlip ))
 echo "Percentage Of Tail - "$percentage_of_tail
+
+echo "*To Store Doublet Combination In A Dictionary And To Determine Its Percentage*"
+
+
+declare -A doublet
+tail=0;
+HH=0;
+TT=0;
+TH=0;
+HT=0;
+
+for((indexOne=0; indexOne<coinFlip; indexOne++))
+do
+
+		flipOne=$((RANDOM%2))
+		flipTwo=$((RANDOM%2))
+		if [[ $flipOne -eq $head ]] &&  [[ $flipTwo -eq $head ]]
+		then
+			doublet[HH]="HH"
+			 ((++HH))
+		elif [[ $flipOne -eq $tail ]] && [[ $flipTwo -eq $tail ]]
+		then
+			 doublet[TT]="TT"
+			  ((++TT))
+		elif [[ $flipOne -eq $tail ]]  && [[ $flipTwo -eq $head ]]
+		then
+			doublet[TH]="TH"
+			 ((++TH))
+		else
+			doublet[HT]="HT"
+			 ((++HT))
+		fi
+		echo "Doublet Combination - " ${doublet[@]}
+done
+
+percentage_of_HH=$(($HH*100/$coinFlip))
+echo "Percentage Of HH - "$percentage_of_HH
+percentage_of_TT=$(( $TT*100/$coinFlip ))
+echo "Percentage Of TT - "$percentage_of_TT
+percentage_of_TH=$(($TH*100/$coinFlip))
+echo "Percentage of TH - "$percentage_of_TH
+percentage_of_HT=$(($HT*100/$coinFlip))
+echo "Percentage of HT - "$percentage_of_HT
+
